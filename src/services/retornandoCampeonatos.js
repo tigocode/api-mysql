e1552f2fd9243c68836ef319b40b4e86a4a8e754
tigocode <tiago.dantas16@live.com>
2024-12-25 19:13:45 -0300

    :recycle: refactor: melhorando o codigo das functions

--- src/services/retornandoCampeonatos.js
+++ src/services/retornandoCampeonatos.js
@@ -1,50 +1,49 @@
 const pool = require('./conection');
 
-
-const retornandoCampenatos =  async () => {
+const executeQuery = async (query) => {
   const connection = await pool.getConnection();
 
-  const campeonates_tb = await connection.query('select * from campeonatos');
+  const result_query = await connection.query(query);
 
-  const campeonatos = campeonates_tb[0];
+  const resposta = result_query[0];
 
   connection.release();
+
+  return resposta;
+}
+
+const retornandoCampenatos =  async () => { 
+
+  const query = 'select * from campeonatos';
+
+  const campeonatos = executeQuery(query);  
 
   return campeonatos;
 }
 
 const retornandoCampenatosID =  async (id) => {
-  const connection = await pool.getConnection();
 
-  const campeonates_tb = await connection.query(`select * from campeonatos where id = ${id}`);
+  const query = `select * from campeonatos where id = ${id}`;
 
-  const campeonatos = campeonates_tb[0];
-
-  connection.release();
+  const campeonatos = executeQuery(query);  
 
   return campeonatos;
 }
 
 const retornandoCampenatosAno =  async (ano) => {
-  const connection = await pool.getConnection();
 
-  const campeonates_tb = await connection.query(`select * from campeonatos where ano = ${ano}`);
+  const query = `select * from campeonatos where ano = ${ano}`;
 
-  const campeonatos = campeonates_tb[0];
-
-  connection.release();
+  const campeonatos = executeQuery(query);  
 
   return campeonatos;
 }
 
 const retornandoCampenatosTime =  async (time) => {
-  const connection = await pool.getConnection();
 
-  const campeonates_tb = await connection.query(`select * from campeonatos where campeao = ${time}`);
+  const query = `select * from campeonatos where campeao = ${time}`;
 
-  const campeonatos = campeonates_tb[0];
-
-  connection.release();
+  const campeonatos = executeQuery(query);  
 
   return campeonatos;
 }
